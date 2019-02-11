f0a8676b0886b0ef9fb78411ef61265cda7d38cb
Tiago Guimarães <tilacog@gmail.com>
2019-02-11 13:33:53 -0200

    Add Smart Contract Functions:
    lookUptokenIdToStarInfo, exchangeStars, transferStar

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -17,7 +17,6 @@ contract StarNotary is ERC721 {
     // symbol: Is a short string like 'USD' -> 'American Dollar'
     string public constant symbol = "TIS";
 
-
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
@@ -57,22 +56,31 @@ contract StarNotary is ERC721 {
     }
 
     // Implement Task 1 lookUptokenIdToStarInfo
-    function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
+    function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory starname) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
+        starname = tokenIdToStarInfo[_tokenId].name;
+        return starname;
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        require(ownerOf(_tokenId1) == msg.sender || ownerOf(_tokenId2) == msg.sender);
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
+	_transferFrom(ownerOf(_tokenId1),ownerOf(_tokenId2), _tokenId1);
+        _transferFrom(ownerOf(_tokenId2),ownerOf(_tokenId1), _tokenId2);
+
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
+      require(ownerOf(_tokenId) == msg.sender);
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+      _transferFrom(ownerOf(_tokenId), _to1, _tokenId);
+
     }
 
 }

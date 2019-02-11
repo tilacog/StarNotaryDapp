4014a7ace5548c24e0be3544298983fb71ed13a5
Tiago Guimarães <tilacog@gmail.com>
2019-02-11 13:25:34 -0200

    Add Smart Contract Functions: Add a name and a symbol to the starNotary tokens

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -13,15 +13,17 @@ contract StarNotary is ERC721 {
 
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
+    string public constant name = "TiagoStar";
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string public constant symbol = "TIS";
+
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-    
+
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
